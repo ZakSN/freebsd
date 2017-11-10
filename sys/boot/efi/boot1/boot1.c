15b23c5bec209ed2d908c47e45fbef58ec930779
Zakary Nafziger <Zakary Nafziger>
2017-11-10 14:49:37 -0500

    trim whitespace

--- sys/boot/efi/boot1/boot1.c
+++ sys/boot/efi/boot1/boot1.c
@@ -464,7 +464,7 @@ efi_main(EFI_HANDLE Ximage, EFI_SYSTEM_TABLE *Xsystab)
 			printf("   Load Path: %S\n", text);
 			efi_setenv_freebsd_wcs("Boot1Path", text);
 			efi_free_devpath_name(text);
-	
+
 			status = BS->HandleProtocol(img->DeviceHandle, &DevicePathGUID,
 			    (void **)&imgpath);
 			if (status != EFI_SUCCESS) {
@@ -477,7 +477,7 @@ efi_main(EFI_HANDLE Ximage, EFI_SYSTEM_TABLE *Xsystab)
 				efi_free_devpath_name(text);
 			}
 		}
-	
+
 	}
 
 	/* Get all the device handles */
